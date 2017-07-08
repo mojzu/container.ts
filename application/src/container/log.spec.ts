58b85354d7ff10f9299fdd0aecffae99dfc6a477
mojzu <mail@mojzu.net>
2017-07-08 19:03:12 +0100

    test: update log module tests

--- application/src/container/log.spec.ts
+++ application/src/container/log.spec.ts
@@ -15,91 +15,99 @@ describe("Log", () => {
 
   const LOGGER = new TestLogger();
 
-  it("#Logger#emergency", () => {
+  it("#Logger#emergency", (done) => {
     const error = new Error("Emergency");
     const metadata = { value: 1 };
     LOGGER.emergency(error, metadata, (level: LogLevel, logError: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Emergency);
       expect(logError).toEqual(error);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#alert", () => {
+  it("#Logger#alert", (done) => {
     const error = new Error("Alert");
     const metadata = { value: 1 };
     LOGGER.alert(error, metadata, (level: LogLevel, logError: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Alert);
       expect(logError).toEqual(error);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#critical", () => {
+  it("#Logger#critical", (done) => {
     const error = new Error("Critical");
     const metadata = { value: 1 };
     LOGGER.critical(error, metadata, (level: LogLevel, logError: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Critical);
       expect(logError).toEqual(error);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#error", () => {
+  it("#Logger#error", (done) => {
     const error = new Error("Error");
     const metadata = { value: 1 };
     LOGGER.error(error, metadata, (level: LogLevel, logError: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Error);
       expect(logError).toEqual(error);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#warn", () => {
+  it("#Logger#warn", (done) => {
     const message = "Warning";
     const metadata = { value: 1 };
     LOGGER.warn(message, metadata, (level: LogLevel, logMessage: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Warning);
       expect(logMessage).toEqual(message);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#notice", () => {
+  it("#Logger#notice", (done) => {
     const message = "Notice";
     const metadata = { value: 1 };
     LOGGER.notice(message, metadata, (level: LogLevel, logMessage: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Notice);
       expect(logMessage).toEqual(message);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#info", () => {
+  it("#Logger#info", (done) => {
     const message = "Informational";
     const metadata = { value: 1 };
     LOGGER.info(message, metadata, (level: LogLevel, logMessage: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Informational);
       expect(logMessage).toEqual(message);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
-  it("#Logger#debug", () => {
+  it("#Logger#debug", (done) => {
     const message = "Debug";
     const metadata = { value: 1 };
     LOGGER.debug(message, metadata, (level: LogLevel, logMessage: ILogMessage, logMetadata: ILogMetadata) => {
       expect(level).toEqual(LogLevel.Debug);
       expect(logMessage).toEqual(message);
-      expect(metadata).toBeDefined();
-      expect(metadata.value).toEqual(logMetadata.value);
+      expect(logMetadata).toBeDefined();
+      expect(logMetadata.value).toEqual(metadata.value);
+      done();
     });
   });
 
---
+++ application/src/modules/log/log.spec.ts
@@ -0,0 +1,73 @@
+/// <reference types="jasmine" />
+import { ContainerLogMessage, Container, LogLevel } from "../../container";
+import { Log } from "./log";
+
+type ITestLogCallback = (log: ContainerLogMessage) => void;
+
+class TestLog extends Log {
+  protected handleLog(log: ContainerLogMessage): void {
+    const callback: ITestLogCallback = log.args[0];
+    if (callback != null) {
+      callback(log);
+    }
+  }
+}
+
+describe("Log", () => {
+
+  const NAME = "test";
+  const MODULE_NAME = "log";
+
+  const CONTAINER = new Container(NAME)
+    .registerModule(MODULE_NAME, TestLog);
+
+  const LOG = CONTAINER.resolve<TestLog>(MODULE_NAME);
+
+  beforeAll((done) => {
+    CONTAINER.start()
+      .subscribe({
+        next: () => done(),
+        error: (error) => done.fail(error),
+      });
+  });
+
+  afterAll((done) => {
+    CONTAINER.stop()
+      .subscribe({
+        next: () => done(),
+        error: (error) => done.fail(error),
+      });
+  });
+
+  it("#TestLog", () => {
+    expect(LOG).toBeDefined();
+    expect(LOG.name).toEqual(MODULE_NAME);
+  });
+
+  it("#TestLog#emergency", (done) => {
+    const error = new Error("Emergency");
+    const metadata = { value: 1 };
+    LOG.log.emergency(error, metadata, (log: ContainerLogMessage) => {
+      expect(log.level).toEqual(LogLevel.Emergency);
+      expect(log.message).toEqual(error);
+      expect(log.metadata).toBeDefined();
+      expect(log.metadata.value).toEqual(metadata.value);
+      expect(log.metadata.moduleName).toEqual(`${NAME}:${MODULE_NAME}`);
+      done();
+    });
+  });
+
+  it("#TestLog#critical", (done) => {
+    const error = new Error("Critical");
+    const metadata = { value: 1 };
+    LOG.log.critical(error, metadata, (log: ContainerLogMessage) => {
+      expect(log.level).toEqual(LogLevel.Critical);
+      expect(log.message).toEqual(error);
+      expect(log.metadata).toBeDefined();
+      expect(log.metadata.value).toEqual(metadata.value);
+      expect(log.metadata.moduleName).toEqual(`${NAME}:${MODULE_NAME}`);
+      done();
+    });
+  });
+
+});
