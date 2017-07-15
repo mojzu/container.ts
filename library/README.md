bb996518c9628640a20a22b8f7c271cfa53f162f
mojzu <mail@mojzu.net>
2017-07-15 21:26:07 +0100

    chore: add library package gulp tasks

--- library/README.md
+++ library/README.md
@@ -1,1 +1,11 @@
 # Library
+
+## Developer
+
+```Shell
+# Install dependencies.
+$ yarn install
+
+# Run tasks defined in `gulpfile.js`.
+$ yarn run clean|distclean|test|lint|docs|build
+```
--- library/gulpfile.js
+++ library/gulpfile.js
@@ -3,6 +3,7 @@ const path = require("path");
 const gulp = require("gulp");
 const gutil = require("gulp-util");
 const clean = require("./gulp/clean");
+const shell = require("./gulp/shell");
 
 // Library root directory.
 const PATH = path.resolve(__dirname);
@@ -22,4 +23,29 @@ gulp.task("clean", (done) => {
 // Clean and delete modules.
 gulp.task("distclean", ["clean"], (done) => {
   clean.run(PATH, ["node_modules"], done);
+});
+
+// Run TypeScript compiler.
+gulp.task("tsc", ["clean"], (done) => {
+  shell.run("tsc", PATH, done);
+});
+
+// Run tests with coverage reporting.
+gulp.task("test", ["tsc"], (done) => {
+  shell.run("istanbul cover test.js -x \"**/*.spec.js\"", PATH, done);
+});
+
+// Run linter.
+gulp.task("lint", (done) => {
+  shell.run("tslint -p tsconfig.json --type-check", PATH, done);
+});
+
+// Build documentation.
+gulp.task("docs", (done) => {
+  shell.run("typedoc --out docs/typedoc .", PATH, done);
+});
+
+// Build library.
+gulp.task("build", ["tsc"], (done) => {
+  shell.run("npm pack", PATH, done);
 });
