312a0c382c1089b325125e77f25c294f3a960308
mojzu <mail@mojzu.net>
2017-07-15 21:50:32 +0100

    fix: fix library package clean task

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -5,47 +5,47 @@ const gutil = require("gulp-util");
 const clean = require("./gulp/clean");
 const shell = require("./gulp/shell");
 
-// Library root directory.
-const PATH = path.resolve(__dirname);
+// Library package path and file.
+const packagePath = path.resolve(__dirname);
+const packageJson = require("./package.json");
 
 // Delete compiled files.
 gulp.task("clean", (done) => {
-  clean.run(PATH, [
+  clean.run(packagePath, [
     "*.tgz",
     "*.log",
-    "**/*.d.ts",
-    "**/*.js.map",
-    "index.js",
+    "test.d.ts",
+    "test.js.map",
     "test.js",
-  ], done);
+  ].concat(packageJson.files), done);
 });
 
-// Clean and delete modules.
+// Clean and delete modules generated documentation.
 gulp.task("distclean", ["clean"], (done) => {
-  clean.run(PATH, ["node_modules"], done);
+  clean.run(packagePath, ["docs/typedoc", "node_modules"], done);
 });
 
 // Run TypeScript compiler.
 gulp.task("tsc", ["clean"], (done) => {
-  shell.run("tsc", PATH, done);
+  shell.run("tsc", packagePath, done);
 });
 
 // Run tests with coverage reporting.
 gulp.task("test", ["tsc"], (done) => {
-  shell.run("istanbul cover test.js -x \"**/*.spec.js\"", PATH, done);
+  shell.run("istanbul cover test.js -x \"**/*.spec.js\"", packagePath, done);
 });
 
 // Run linter.
 gulp.task("lint", (done) => {
-  shell.run("tslint -p tsconfig.json --type-check", PATH, done);
+  shell.run("tslint -p tsconfig.json --type-check", packagePath, done);
 });
 
 // Build documentation.
 gulp.task("docs", (done) => {
-  shell.run("typedoc --out docs/typedoc .", PATH, done);
+  shell.run("typedoc --out docs/typedoc .", packagePath, done);
 });
 
 // Build library.
 gulp.task("build", ["tsc"], (done) => {
-  shell.run("npm pack", PATH, done);
+  shell.run("npm pack", packagePath, done);
 });
