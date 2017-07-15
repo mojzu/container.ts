e1c8b59e0d34a950284111c708ba2b5bc3f07394
mojzu <mail@mojzu.net>
2017-07-15 21:01:39 +0100

    feat: library initial commit

---
+++ library/test.ts
@@ -0,0 +1,11 @@
+/// <reference types="node" />
+const Jasmine = require("jasmine");
+const specs = new Jasmine();
+
+// Run tests for compiled `*.spec.js` files
+specs.loadConfig({
+  spec_dir: "dist",
+  spec_files: ["**/*[spec].js"],
+});
+specs.configureDefaultReporter({ showColors: true });
+specs.execute();
