50248a2176e078ed99c4293f26bb36f8abc8fb66
mojzu <mail@mojzu.net>
2017-08-03 23:37:39 +0100

    chore: planned wrapper modules

--- library/modules/index.ts
+++ library/modules/index.ts
@@ -8,6 +8,8 @@ export * from "./Script";
 export * from "./ScriptManager";
 // Package wrapper modules.
 export * from "./wrappers/CronScheduler";
+export * from "./wrappers/MongooseDatabase";
+export * from "./wrappers/NodemailerEmail";
 export * from "./wrappers/RedisCache";
 export * from "./wrappers/RestifyServer";
 export * from "./wrappers/RollbarLog";
---
+++ library/modules/wrappers/MongooseDatabase.ts
@@ -0,0 +1,11 @@
+/// <reference types="node" />
+import { IContainerModuleOpts, ContainerModule } from "../../container";
+
+export class MongooseDatabase extends ContainerModule {
+
+  public constructor(name: string, opts: IContainerModuleOpts) {
+    super(name, opts);
+    // TODO: Implement this.
+  }
+
+}
